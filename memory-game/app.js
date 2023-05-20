37b73d5fd1580b250f346fd05fc9f894676d00c6
bismarkboateng <bismarkb609@gmail.com>
2023-05-20 23:38:05 +0000

    array

---
+++ memory-game/app.js
@@ -0,0 +1,59 @@
+const cardArray = [
+    { 
+        name: "fries",
+        icon: "images/fries.png"
+    },
+    {
+        name: "chessburger",
+        icon: "images/cheeseburger.png"
+    },
+    {
+        name: "hotdog",
+        icon: "images/hotdog.png"
+    },
+    {
+        name: "ice-cream",
+        icon: "images/ice-cream.png"
+    },
+    {
+        name: "milkshake",
+        icon: "images/milkshake.png"
+    },
+    {
+        name: "pizza",
+        icon: "images/pizza.png"
+    },
+    { 
+        name: "fries",
+        icon: "images/fries.png"
+    },
+    {
+        name: "chessburger",
+        icon: "images/cheeseburger.png"
+    },
+    {
+        name: "hotdog",
+        icon: "images/hotdog.png"
+    },
+    {
+        name: "ice-cream",
+        icon: "images/ice-cream.png"
+    },
+    {
+        name: "milkshake",
+        icon: "images/milkshake.png"
+    },
+    {
+        name: "pizza",
+        icon: "images/pizza.png"
+    }
+]
+
+
+const grid = document.getElementById("grid")
+// looping through the cardArray and creating an element
+cardArray.forEach((item, i) => {
+    cardImage = document.createElement("img")
+    cardImage.setAttribute("src", item.icon)
+    console.log(cardImage);
+});
