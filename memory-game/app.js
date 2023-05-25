17672c8cdd5e27764abbe572cc0b3f218af65884
bismarkboateng <bismarkb609@gmail.com>
2023-05-25 21:04:09 +0000

    memory game complete

--- memory-game/app.js
+++ memory-game/app.js
@@ -50,18 +50,47 @@ const cardArray = [
 ]
 
 
-const grid = document.getElementById("grid")
-// looping through the cardArray and creating an element
+// first create a function to append all the images in the array 
+// under the dom node #grid
+// add click even listeners to each of the images 
+
+// on the call to the event listener, create an array to store the clicked images
+// get their ids, and flip the images by resetting their src attributes 
+// to the required images using array indexing
 
 
-const createBoard = () => {
-    cardArray.forEach((item, i) => {
-        cardImage = document.createElement("img")
+// for checking a match, we need to look for both ids and see if they are equal
+// if so, we alert the use for a win, we reset the attributes to blank images and remove the event listeners
+
+
+// selecting dom elements
+const gridDisplay = document.getElementById("grid")
+const cardChosen = []
+
+const Board = () => {
+    cardArray.forEach((item, id) => {
+        const cardImage = document.createElement("img")
         cardImage.setAttribute("src", "images/blank.png")
-        cardImage.setAttribute("data-id", i)
+        cardImage.setAttribute("data-id", id)
+
+        // adding an even listener
         cardImage.addEventListener("click", flipCard);
-        grid.appendChild(cardImage)
+        gridDisplay.appendChild(cardImage)
     });
 }
 
-createBoard();
+
+const flipCard = () => {
+    const cardChosenId = this.getAttribute("data-id")
+    cardChosen.push(cardChosenId)
+    this.setAttribute("src", cardArray[cardChosenId].icon) // fliping the image
+
+    if (cardChosen.length == 2){
+        if (cardChosen[0] === cardChosen[1]) {
+            alert("You win")
+            this.setAttribute("src", "images/white.png") // resetting the image to white
+        }
+    }
+}
+
+Board();
