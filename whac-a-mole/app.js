3bdfe93df3b16101d102d1ddf80c0e6823e342da
bismarkboateng <bismarkb609@gmail.com>
2023-05-29 20:42:17 +0000

    move mole, add listeners

--- whac-a-mole/app.js
+++ whac-a-mole/app.js
@@ -1,8 +1,40 @@
-const allBoxElement = document.querySelectorAll(".box")
+const squares = document.querySelectorAll(".square")
+const score = document.getElementById("score-result")
+const timer = document.getElementById("timer-left")
 
-allBoxElement.forEach((box) => {
-    box.addEventListener("click", () => {
-        // document.body.style.backgroundClip = "red";
-        console.log("Worked tho")
+let timerId = null
+let result = 0
+let randomSq
+let hitPosition
+timer.textContent = 60
+score.textContent = 0
+// timer = 60
+
+const randomSquare = () => {
+
+    squares.forEach(square => {
+        square.classList.remove("mole")
+
     })
-});
+
+    randomSq = squares[Math.floor(Math.random() * 9)]
+    hitPosition = randomSq.id
+    randomSq.classList.add("mole")
+}
+
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 500)
+}
+
+
+moveMole()
+
+squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+        if (square.id === hitPosition){
+            result++
+            score.textContent = result
+        }
+    })
+})
