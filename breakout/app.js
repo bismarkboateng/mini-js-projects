24f8b3d047ef79dc82d17248728754c214c8c452
bismarkboateng <bismarkb609@gmail.com>
2023-05-31 21:35:21 +0000

    block class

--- breakout/app.js
+++ breakout/app.js
@@ -1,3 +1,14 @@
 const grid = document.querySelector(".grid")
 
-console.log(grid)
+const blockWidth = 100
+const blockHeight = 30
+
+// class to create a block
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
